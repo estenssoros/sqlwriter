57efec950871b2c5fa60248ea51a8c90b123dca9
Sebastian Estenssoro <estenssoros@Sebastians-MacBook-Pro.local>
2017-12-30 09:38:00 -0700

    added pyflakes check script

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
+import subprocess as sp
+import sys
+
 from setuptools import setup
 
 
+def check():
+    """Check the source code to make sure there are no syntax failures"""
+    cmd = "python -m pyflakes ."
+    p = sp.Popen(cmd, shell=True, stdout=sp.PIPE)
+    p.wait()
+    out, err = p.communicate()
+    if out:
+        print('PYFLAKES:')
+        print(out)
+        sys.exit(1)
+
+
 def do_setup():
+    check()
     setup(
         name='sqlwriter',
         description="Writes pandas DataFrame to several flavors of sql database",
         license="MIT License",
         version='1.0.0',
-        packages=['sqlwriter',],
+        packages=['sqlwriter', ],
         install_requires=[
             'pandas==0.20.3',
             'tqdm==4.19.1',
