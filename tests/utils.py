23e32fb71ca405ccf11b66fa70d84891274ec4e5
Sebastian Estenssoro <sestenssoro@eogresources.com>
2018-01-23 12:37:45 -0700

    added absolute path

--- tests/utils.py
+++ tests/utils.py
@@ -1,8 +1,12 @@
+import os
+
 import yaml
+
+this_dir = os.path.dirname(__file__)
 
 
 def get_config(prog=None):
-    cfg_file = 'test_conf.yaml'
+    cfg_file = os.path.join(this_dir, 'conf.yaml')
 
     with open(cfg_file, 'r') as f:
         config = yaml.load(f)
