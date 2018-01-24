eaec35204622f61fdfaf351cedae1d261cf98bad
Sebastian Estenssoro <sestenssoro@eogresources.com>
2018-01-24 08:08:13 -0700

    test inset postgres

--- tests/test_connections.py
+++ tests/test_connections.py
@@ -104,8 +104,28 @@ class TestInsertMySQL(unittest.TestCase):
     def test_insert_mysql(self):
         curs, conn = self.db['mysql']
         writer = SQLWriter(conn, 'sqlwriter', 'test', self.df.columns)
+
         writer.write(self.df.values)
         writer.close()
+
+    def tearDown(self):
+        self.db.close()
+
+
+class TestInsertPostGres(unittest.TestCase):
+    def setUp(self):
+        self.df = create_test_dataframe()
+        self.db = DBRouter('postgres')
+
+    def test_insert_mysql(self):
+        curs, conn = self.db['postgres']
+        writer = SQLWriter(conn, 'sqlwriter', 'test', self.df.columns)
+
+        writer.write(self.df.values)
+        writer.close()
+
+    def tearDown(self):
+        self.db.close()
 
 
 if __name__ == '__main__':
