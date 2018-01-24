c07200a682aea8babb8b6eb097f98e5b16a698e1
Sebastian Estenssoro <sestenssoro@eogresources.com>
2018-01-24 10:22:51 -0700

    added mssql and broke insert and create into separate modules

--- tests/test_connections.py
+++ tests/test_connections.py
@@ -1,9 +1,7 @@
 import subprocess as sp
 import unittest
 
-from sqlwriter import SQLWriter
-
-from utils import DBRouter, connect_db, create_test_dataframe
+from utils import connect_db
 
 p = sp.Popen('docker ps', shell=True, stdout=sp.PIPE)
 p.wait()
@@ -11,11 +9,14 @@ msg, _ = p.communicate()
 
 SKIP_MYSQL = True
 SKIP_POSTGRES = True
+SKIP_MSSQL = True
 
 if 'sqlwriter_postgres_1' in msg:
     SKIP_POSTGRES = False
 if 'sqlwriter_mysql_1' in msg:
     SKIP_MYSQL = False
+if 'sqlwriter_mssql_1' in msg:
+    SKIP_MSSQL = False
 
 
 class TestDBConnections(unittest.TestCase):
@@ -42,90 +43,16 @@ class TestDBConnections(unittest.TestCase):
             print(e)
             self.assertTrue(False)
 
-
-class TestCreatePostgres(unittest.TestCase):
-    def setUp(self):
-        self.db = DBRouter('postgres')
-
-    def test_create_table(self):
-        curs, conn = self.db['postgres']
-        curs.execute('DROP TABLE IF EXISTS test')
-        conn.commit()
-        sql = '''
-        CREATE TABLE test (
-            id SERIAL
-            , astring VARCHAR(50)
-            , aninteger INTEGER
-            , afloat FLOAT
-            , adate DATE
-            , adatetime TIMESTAMP WITHOUT TIME ZONE
-        )
-        '''
-        curs.execute(sql)
-        conn.commit()
-
-    def tearDown(self):
-        self.db.close()
-
-
-class TestCreateMySQL(unittest.TestCase):
-    def setUp(self):
-        self.db = DBRouter('mysql')
-
-    def test_create_dbmysql(self):
-        curs, conn = self.db['mysql']
-        curs.execute('CREATE DATABASE IF NOT EXISTS sqlwriter')
-        conn.commit()
-
-        curs.execute('DROP TABLE IF EXISTS sqlwriter.test')
-        conn.commit()
-        sql = '''
-        CREATE TABLE test (
-            id SERIAL
-            , astring VARCHAR(50)
-            , aninteger INTEGER
-            , afloat FLOAT
-            , adate DATE
-            , adatetime DATETIME
-        )
-        '''
-        curs.execute(sql)
-        conn.commit()
-
-    def tearDown(self):
-        self.db.close()
-
-
-class TestInsertMySQL(unittest.TestCase):
-    def setUp(self):
-        self.df = create_test_dataframe()
-        self.db = DBRouter('mysql')
-
-    def test_insert_mysql(self):
-        curs, conn = self.db['mysql']
-        writer = SQLWriter(conn, 'sqlwriter', 'test', self.df.columns)
-
-        writer.write(self.df.values)
-        writer.close()
-
-    def tearDown(self):
-        self.db.close()
-
-
-class TestInsertPostGres(unittest.TestCase):
-    def setUp(self):
-        self.df = create_test_dataframe()
-        self.db = DBRouter('postgres')
-
-    def test_insert_mysql(self):
-        curs, conn = self.db['postgres']
-        writer = SQLWriter(conn, 'sqlwriter', 'test', self.df.columns)
-
-        writer.write(self.df.values)
-        writer.close()
-
-    def tearDown(self):
-        self.db.close()
+    @unittest.skipIf(SKIP_MSSQL, 'Could not find running MsSQL docker container')
+    def test_mssql_can_connect(self):
+        try:
+            curs, conn = connect_db('mssql')
+            self.assertTrue(True)
+            curs.close()
+            conn.close()
+        except Exception as e:
+            print(e)
+            self.assertTrue(False)
 
 
 if __name__ == '__main__':
