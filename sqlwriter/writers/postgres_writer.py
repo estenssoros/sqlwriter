5fdcace55f68f4dfd82a8349b2bb30210f6f0ba5
Sebastian Estenssoro <sestenssoro@eogresources.com>
2018-01-24 08:07:51 -0700

    since postgres only allows you one database, had to remove db.table syntax

--- sqlwriter/writers/postgres_writer.py
+++ sqlwriter/writers/postgres_writer.py
@@ -3,11 +3,15 @@ from sqlwriter.writers.base_writer import BaseWriter
 
 class PostGresWriter(BaseWriter):
     def __init__(self, *args, **kwargs):
-        super(MsSQLWriter, self).__init__(*args, **kwargs)
+        super(PostGresWriter, self).__init__(*args, **kwargs)
+
+    @property
+    def insert_part(self):
+        return 'INSERT INTO {} ('.format(self.table_name) + ','.join(self.cols) + ') VALUES '
 
     @property
     def description(self):
-        self.curs.execute('SELECT %s FROM %s LIMIT 1' % (','.join(self.cols), self.db_table))
+        self.curs.execute('SELECT %s FROM %s LIMIT 1' % (','.join(self.cols), self.table_name))
         return self.curs.description
 
     def _make_fields(self):
